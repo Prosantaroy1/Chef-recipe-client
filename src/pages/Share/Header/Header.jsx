c27af12963c911d76ecb45a04d53b79b4c960637
Prosantaroy1 <royprosanta172@gmail.com>
2023-05-02 22:18:23 +0600

    banner design done

--- src/pages/Share/Header/Header.jsx
+++ src/pages/Share/Header/Header.jsx
@@ -1,17 +1,80 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
 import NavBar from '../NavBar/NavBar';
+import './Header.css'
+import { Button } from 'react-bootstrap';
+import {  AiOutlineArrowLeft } from 'react-icons/ai';
 
 const Header = () => {
     return (
         <div>
-            <NavBar></NavBar>
-            <Container>
-              
-            </Container>
+            <div className='sticky-top'>
+              <NavBar></NavBar>
+            </div>
+            <div className='banner-img text-light d-flex flex-column align-items-center 
+            justify-content-center lh-lg'>
+               
+                   <h2 className='text-center fw-bold text-light'>
+                     BEST BANGLADESHI RECIPES 
+                    <br/> <span className='text-dark'>বাংলাদেশী রেসিপি</span>
+                  </h2>
+                   <p className='text-center fw-bold text-success lh-base'>
+                    স্বাদে পরিপূর্ণ আহারের জন্য আমাদের রেসিপি সাইট দেখুন!<br/> এখানে আপনি বিভিন্ন ধরনের 
+                     রেসিপি 
+                    পাবেন।<br/>
+                   আমাদের সাইটে আপনি সহজেই সেই রেসিপি খুঁজে পাবেন। সহজ নির্দেশনা 
+                    এবং পরামর্শের <br/>
+                    সাহায্যে আপনি সুস্বাদু খাবার তৈরি করতে পারবেন।
+                   </p>
+                   <Button variant="warning"  className='px-4 py-2 fw-bold'>
+                   <AiOutlineArrowLeft />  Choice Recipe </Button>
+                  {' '}
+           </div>
             
         </div>
     );
 };
 
 export default Header;
+/*
+  <Carousel slide={false}>
+                    <Carousel.Item>
+                    
+                      <img
+                        className="w-100"
+                        src={img1}
+                        alt="First slide"
+                      />
+                     
+                      <Carousel.Caption>
+                        <h2>First slide label</h2>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                      </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                      <img
+                        className="d-block w-100 h-25"
+                        src={img2}
+                        alt="Second slide"
+                      />
+                
+                      <Carousel.Caption >
+                        <h2>Second slide label</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                      </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                      <img
+                        className="d-block w-100 h-25"
+                        src={img3}
+                        alt="Third slide"
+                      />
+                
+                      <Carousel.Caption>
+                        <h2>Third slide label</h2>
+                        <p>
+                          Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                        </p>
+                      </Carousel.Caption>
+                    </Carousel.Item>
+                 </Carousel>
+                 */
--- src/pages/Share/NavBar/NavBar.jsx
+++ src/pages/Share/NavBar/NavBar.jsx
@@ -4,22 +4,26 @@ import { Link } from 'react-router-dom';
 
 const NavBar = () => {
     return (
-        <Container>
-              <Navbar collapseOnSelect expand="lg" bg="light" variant="light" className='mt-2 mb-4'>
+        <div class="p-2 bg-light  " >
+         <Container>
+              <Navbar collapseOnSelect expand="lg"  variant="light" >
                <Container>
-               <Nav.Link href="#pricing" className='fs-3 fw-bold  text-primary'>Bangli-CheF</Nav.Link>
+               <Nav.Link href="#pricing" className='fs-3 fw-bold text-primary'>Bangli-CheF</Nav.Link>
                  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                  <Navbar.Collapse id="responsive-navbar-nav">
                    <Nav className="mx-auto">
-                     <Nav.Link className='fs-5 fw-600'>
-                         <Link to="/" className='text-decoration-none'>Home</Link>
+                     <Nav.Link className='fs-4 fw-semibold'>
+                         <Link to="/" className='text-decoration-none text-dark'>Home</Link>
                      </Nav.Link>
-                     <Nav.Link href="#pricing" className='fs-5 fw-600'>
-                        <Link to="/about" className='text-decoration-none'>About</Link>
+                     <Nav.Link href="#pricing" className='fs-4 fw-semibold'>
+                        <Link to="/about" className='text-decoration-none text-dark'>About</Link>
+                     </Nav.Link>
+                     <Nav.Link href="#pricing" className='fs-4 fw-semibold'>
+                        <Link to="/about" className='text-decoration-none text-dark'>Contact</Link>
                      </Nav.Link>
                    </Nav>
-                   <Nav>
-                          
+                   <Nav className='gap-2'>
+                     <Button variant="secondary">Profile</Button>{' '}
                           <Link to="/login">
                              <Button variant="secondary">Login</Button>{' '}
                           </Link>
@@ -28,6 +32,7 @@ const NavBar = () => {
                </Container>
              </Navbar>
         </Container>
+        </div>
     );
 };
 
